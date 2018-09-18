38809e1d9c81562a9ccaa49abc259d5f11c25ddd
jan.adamczyk <jan.adamczyk@LT250148.pns.local>
2018-09-18 11:52:22 +0200

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+/* 
+ *  Copyright (C) Jan Adamczyk (j_adamczyk@hotmail.com) 2017
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/* 
+ *  Copyright (C) Jan Adamczyk (j_adamczyk@hotmail.com) 2017
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
